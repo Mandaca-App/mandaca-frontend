962141066f9c56414e74a38c346eb5ba4b14db2b
Hivison <84513910+FuscaRetratil@users.noreply.github.com>
2026-03-27 20:11:54 +0000

    fix: max len fixed

--- components/Home/routeGrid/main.tsx
+++ components/Home/routeGrid/main.tsx
@@ -7,8 +7,8 @@ export const RouteGrid = ()=> {
         <View className="gap-4">
             <GridBox icon={'storefront'} title="Minha Empresa" description="Descrição e cadastro" 
             route={()=> router.navigate('/myBusiness')}/>
-            <GridBox icon={'analytics-sharp'} title="Relatórios" description="Visão geral e Insights" 
-            route={()=> router.navigate('/report')}/>
+            <GridBox icon={'analytics-sharp'} title="Relatórios" description="Visão geral e 
+            Insights" route={()=> router.navigate('/report')}/>
             <GridBox icon={'settings'} title="Configurações" description="Personalize o aplicativo" 
             route={()=> router.navigate('/settings')}/>
             <GridBox icon={'hand-left'} title="Ajuda" description="Entre em contato conosco" 
