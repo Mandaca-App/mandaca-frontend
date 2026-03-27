83e5646d8128d2895f33c8fecf9807d72a157ad6
Hivison <84513910+FuscaRetratil@users.noreply.github.com>
2026-03-27 19:01:02 +0000

    fix: remove eslintrc.js centralize in eslint.config

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,39 @@
-// https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
+const typescriptPlugin = require('@typescript-eslint/eslint-plugin');
+const typescriptParser = require('@typescript-eslint/parser');
 
 module.exports = defineConfig([
   expoConfig,
   {
-    ignores: ['dist/*'],
+    plugins: {
+      '@typescript-eslint': typescriptPlugin,
+    },
+    languageOptions: {
+      parser: typescriptParser,
+    },
+    rules: {
+      'max-len': ['error', {
+        code: 100,
+        ignoreUrls: true,
+        ignoreStrings: false,
+        ignoreComments: false,
+        ignoreTemplateLiterals: false,
+      }],
+
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+
+      'no-console': 'warn',
+      'no-debugger': 'error',
+      'eqeqeq': ['error', 'always'],
+      'semi': ['error', 'always'],
+      'quotes': ['error', 'single'],
+      'comma-dangle': ['error', 'always-multiline'],
+    },
+  },
+  {
+    ignores: ['dist/*', 'node_modules/', '.expo/', 'build/'],
   },
 ]);
