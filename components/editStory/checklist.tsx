76fe9c4d298342e88663a584e39e1713a6f5c723
Daniel Santana <danielsantanachastel@gmail.com>
2026-04-06 20:04:21 -0300

    scrum-78 tela de editar história e toggle de escrever ou gravar áudio

---
+++ components/editStory/checklist.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native';
+import ChecklistButton from './checklistButton';
+
+export default function Checklist() {
+    return (
+        <View className='gap-4'>
+            <Text className='text-lg font-semibold'>Dicas para uma boa descrição</Text>
+            <ChecklistButton text='Nome do negócio' check={true}/>
+            <ChecklistButton text='Sua especialidade (produtos/serviços)' check={true}/>
+            <ChecklistButton text='Localização / Bairro' check={false}/>
+            <ChecklistButton text='Sua história ou o diferencial' check={false}/>
+        </View>
+    );
+}
