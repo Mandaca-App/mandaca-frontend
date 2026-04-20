afc9bc52496aee6edf52e4d195686f65f688066d
Daniel Santana <danielsantanachastel@gmail.com>
2026-04-20 17:52:52 -0300

    scrum-51 telas de detalhamento de feedbacks

--- components/report/PositiveFeedbackList.tsx
+++ components/report/PositiveFeedbackList.tsx
@@ -15,28 +15,28 @@ export const PositiveFeedbackList = ({ handlePress }: { handlePress: () => void 
             id: '1',
             text: 'Seu estabelecimento recebeu avaliações positivas com média de 4.8 estrelas este mês.',
             changes: '',
-            automaticChanges: false,
+            automaticChanges: false
         },
         {
             id: '2',
             text: 'A sopa de feijão foi o prato mais pedido do seu estabelecimento, somando 145 pedidos neste mês. Isso representa um aumento de 18% em comparação ao mês anterior.',
             changes: 'Vou destacar a sopa como prato principal no seu cardápio.',
-            automaticChanges: true,
+            automaticChanges: true
         },
         {
             id: '3',
             text: 'Seu tempo médio de entrega reduziu para 28 minutos, melhorando a experiência dos clientes.',
             changes: 'Vou destacar sua agilidade como diferencial no app.',
-            automaticChanges: true,
+            automaticChanges: true
         },
     ]
     return (
-        <View className="gap-6 mt-4">
+        <View className="gap-8 mt-10">
             {feedbacks.map(item => (
                 <CardItemFeedback
                     key={item.id}
                     handlePress={handlePress}
-                    icon={'analytics'}
+                    icon={"analytics"}
                     text={item.text}
                     chages={item.changes}
                     automaticChanges={item.automaticChanges}
